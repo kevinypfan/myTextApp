5982267749da3ce87a19e9d3168450fc5f28ddeb
kevinypfan <kevin_ypfan@yahoo.com.tw>
2017-08-13 18:38:15 +0800

    new posts system setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 var {User} = require('./models/user');
+var {Post} = require('./models/post');
 
 const app = express();
 var mongoose = require('mongoose');
@@ -13,9 +14,18 @@ mongoose.connect('mongodb://localhost:27017/UserTest');
 app.use(bodyParser.json());
 
 const root = __dirname;
-app.use(express.static(root));
+app.use(express.static('public'));
 
-
+app.post('/posts', (req, res) => {
+  var body = _.pick(req.body, ['user','userImg', 'postTitle', 'postImg']);
+  var post = new Post(body);
+  console.log(post)
+  post.save().then((post) => {
+    res.send(post);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
 // POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
@@ -28,12 +38,7 @@ app.post('/users', (req, res) => {
   })
 });
 
-app.get('/note', (req, res) => {
-  console.log("sended")
-  res.send({
-    "name":'hello'
-  });
-})
+
 app.use(fallback('index.html', { root: root }));
 
 app.listen(8000, function () {
